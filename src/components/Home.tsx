f64946568f7c0eb7587c1aa64c748bb7162aaf21
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-12-14 11:10:22 +0630

    menu draft

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,20 +5,20 @@ const Home = () => {
     <section className="w-screen h-screen">
       <div className="w-full h-full bg-[url('./assets/hero-bg.jpg')] bg-cover bg-center">
         <Navbar />
-        <div className="w-full h-full bg-zinc-900 bg-opacity-50">
-          <div className="w-full h-screen grid md:grid-cols-3 p-20 text-white">
-            <div className="w-full flex flex-col justify-center items-center md:col-span-2 md:items-start">
-              <h2 className="text-3xl md:text-5xl font-bold mb-3">
+        <div className="w-full h-full bg-opacity-50 bg-zinc-900">
+          <div className="grid w-full h-screen p-20 text-white md:grid-cols-3">
+            <div className="flex flex-col items-center justify-center w-full md:col-span-2 md:items-start">
+              <h2 className="mb-3 text-3xl font-bold md:text-5xl">
                 Welcome to <span className="text-yellow-200">Restaurantly</span>
               </h2>
-              <p className="text-xl md:text-2xl mb-6">
+              <p className="mb-6 text-xl md:text-2xl">
                 Delivering great food for more than 18 years!
               </p>
-              <div>
-                <button className="border-2 border-yellow-200 rounded-full w-36 px-4 py-2">
+              <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+                <button className="px-4 py-2 border-2 border-yellow-200 rounded-full w-36">
                   Our Menu
                 </button>
-                <button className="border-2 border-yellow-200 rounded-full w-36 ml-4 px-4 py-2">
+                <button className="px-4 py-2 border-2 border-yellow-200 rounded-full w-36">
                   Book A Table
                 </button>
               </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,114 @@
+import { useState } from "react";
+
+const menu = [
+  {
+    name: "Lobster Bisque",
+    price: 5.95,
+    imageURL: "/src/assets/lobster-bisque.jpg",
+    category: "starter",
+    description: "Lorem, deren, trataro, filede, nerada",
+  },
+  {
+    name: "Bread Barrel",
+    price: 6.95,
+    imageURL: "/src/assets/bread-barrel.jpg",
+    category: "specialty",
+    description: "Lorem, deren, trataro, filede, nerada",
+  },
+  {
+    name: "Crab Cake",
+    price: 7.95,
+    imageURL: "/src/assets/cake.jpg",
+    category: "starter",
+    description:
+      "A delicate crab cake served on a toasted roll with lettuce and tartar sauce",
+  },
+  {
+    name: "Caesar Selection",
+    price: 8.95,
+    imageURL: "/src/assets/caesar.jpg",
+    category: "salad",
+    description: "Lorem, deren, trataro, filede, nerada",
+  },
+  {
+    name: "Tuscan Grilled",
+    price: 9.95,
+    imageURL: "/src/assets/tuscan-grilled.jpg",
+    category: "specialty",
+    description:
+      "Grilled chicken with provolone, artichoke hearts, and roasted red pesto",
+  },
+  {
+    name: "Mozzarella Stick",
+    price: 4.95,
+    imageURL: "/src/assets/mozzarella.jpg",
+    category: "starter",
+    description: "Lorem, deren, trataro, filede, nerada",
+  },
+  {
+    name: "Spinach Salad",
+    price: 9.95,
+    imageURL: "/src/assets/spinach-salad.jpg",
+    category: "salad",
+    description:
+      "Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette",
+  },
+  {
+    name: "Greek Salad",
+    price: 9.95,
+    imageURL: "/src/assets/greek-salad.jpg",
+    category: "salad",
+    description: "Lorem, deren, trataro, filede, nerada",
+  },
+  {
+    name: "Lobster Roll",
+    price: 12.95,
+    imageURL: "/src/assets/lobster-roll.jpg",
+    category: "specialty",
+    description:
+      "Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll",
+  },
+];
+
+// const [filteredMenu, setFilteredMenu] = useState(menu);
+
+// const handleClick = (e: any) => {
+//   console.log(e);
+// };
+
 const Menu = () => {
   return (
-    <section className="w-screen h-screen flex items-center justify-center">
-      Menu
+    <section className="w-screen px-6 py-10 h-max bg-zinc-800 md:px-20">
+      <h2 className="mb-4 text-2xl text-zinc-500">Menu</h2>
+      <h3 className="text-3xl font-bold text-yellow-200">
+        Check Our Tasty Menu
+      </h3>
+
+      <div className="flex items-center justify-center gap-5 my-6 text-lg font-semibold text-white ">
+        <button className="hover:text-yellow-200">All</button>
+        <button className="hover:text-yellow-200">Starters</button>
+        <button className="hover:text-yellow-200">Salads</button>
+        <button className="hover:text-yellow-200">Speciality</button>
+      </div>
+
+      <ul className="grid w-full gap-3 text-white md:grid-cols-2 md:gap-6">
+        {menu.map((item: any) => (
+          <li key={item.name} className="flex w-full gap-2">
+            <div className="flex items-center justify-center w-16 h-16 rounded-full bg-zinc-700">
+              <img
+                className="rounded-full w-14 h-14"
+                src={item.imageURL}
+                alt=""
+              />
+            </div>
+            <div className="flex-1">
+              <p>{item.name}</p>
+              <p className="text-sm italic text-gray-400">{item.description}</p>
+            </div>
+            <p>${item.price}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };

895891d7cfa26ca15afafd1b503aa38291432484
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-12-14 15:00:34 +0630

    img path

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -9,16 +9,16 @@ const Gallery = () => {
       <div className="grid mt-6 divide-x-2 divide-y-2 divide-x-reverse divide-y-reverse md:grid-cols-3 lg:grid-cols-4">
         <img
           className="border-b-2 border-r-2 border-white "
-          src="/src/assets/gallery-1.jpg"
+          src={"../src/assets/gallery-1.jpg"}
           alt=""
         />
-        <img src="/src/assets/gallery-2.jpg" alt="" />
-        <img src="/src/assets/gallery-3.jpg" alt="" />
-        <img src="/src/assets/gallery-4.jpg" alt="" />
-        <img src="/src/assets/gallery-5.jpg" alt="" />
-        <img src="/src/assets/gallery-6.jpg" alt="" />
-        <img src="/src/assets/gallery-7.jpg" alt="" />
-        <img src="/src/assets/gallery-8.jpg" alt="" />
+        <img src={"../src/assets/gallery-2.jpg"} alt="" />
+        <img src={"../src/assets/gallery-3.jpg"} alt="" />
+        <img src={"../src/assets/gallery-4.jpg"} alt="" />
+        <img src={"../src/assets/gallery-5.jpg"} alt="" />
+        <img src={"../src/assets/gallery-6.jpg"} alt="" />
+        <img src={"../src/assets/gallery-7.jpg"} alt="" />
+        <img src={"../src/assets/gallery-8.jpg"} alt="" />
       </div>
     </section>
   );
